81193c5ddbac33ba46f7ad33c0d8c38839cc8a5d
Geoffrey Vedernikoff <yefim323@gmail.com>
2014-10-30 17:50:18 -0400

    Using NSArray instead of assignment

---
+++ SurgePurgePlus/Surge.h
@@ -0,0 +1,18 @@
+//
+//  Surge.h
+//  SurgePurgePlus
+//
+//  Created by Geoffrey Vedernikoff on 10/30/14.
+//  Copyright (c) 2014 YAH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Surge : NSObject
+
+@property (nonatomic) int order;
+@property (nonatomic) CGFloat surge;
+@property (nonatomic) CGPoint point;
+
+@end
